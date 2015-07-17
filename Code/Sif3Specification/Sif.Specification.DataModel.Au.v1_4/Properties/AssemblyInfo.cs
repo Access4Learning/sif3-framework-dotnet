6524c4191c6f932e4bf71f701aef401eec4475e8
rafidzal <rafidzal.rafiq@systemic.com.au>
2015-07-17 15:40:37 +0800

    Added beta version of the SIF AU 1.4 data model
    Added a beta version of the SIF AU 1.4 data model to the
    Sif3Specification Solution.

---
+++ Code/Sif3Specification/Sif.Specification.DataModel.Au.v1_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sif.Specification.DataModel.Au")]
+[assembly: AssemblyDescription("SIF AU 1.4 (beta01) Data Model library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Systemic Pty Ltd")]
+[assembly: AssemblyProduct("Sif.Specification.DataModel.Au")]
+[assembly: AssemblyCopyright("Copyright © Systemic Pty Ltd 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1df0994e-8013-457a-a85c-49dc06e92e52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
