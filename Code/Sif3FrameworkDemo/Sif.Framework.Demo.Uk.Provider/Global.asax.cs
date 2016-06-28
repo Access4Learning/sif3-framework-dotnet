125d74040641f9b9466e65defdece2128edb2a1d
Jon Nicholson <jon.nicholson@zinetdatasolutions.com>
2016-06-28 09:46:54 +0100

    Improved logging.

--- Code/Sif3FrameworkDemo/Sif.Framework.Demo.Uk.Provider/Global.asax.cs
+++ Code/Sif3FrameworkDemo/Sif.Framework.Demo.Uk.Provider/Global.asax.cs
@@ -13,19 +13,34 @@ using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using System.Web.Routing;
 using System.Xml.Serialization;
+using Sif.Framework.Providers;
+using log4net;
+using System.Reflection;
 
 namespace Sif.Framework.Demo.Uk.Provider
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private IRegistrationService registrationService;
+        
         private void Register()
         {
-            registrationService = RegistrationManager.ProviderRegistrationService;
-            registrationService.Register();
+            if (registrationService == null)
+            {
+                log.Info("Getting registration manager...");
+                registrationService = RegistrationManager.ProviderRegistrationService;
+            }
+
+            if (!registrationService.Registered)
+            {
+                log.Info("Registering...");
+                registrationService.Register();
+            }
         }
         private void Unregister()
         {
+            log.Info("Unregistering...");
             registrationService.Unregister();
         }
 
@@ -34,6 +49,7 @@ namespace Sif.Framework.Demo.Uk.Provider
 			base.Init();
 			this.AcquireRequestState += showRouteValues;
 		}
+
 		protected void showRouteValues(object sender, EventArgs e)
 		{
 			var context = HttpContext.Current;
@@ -70,11 +86,13 @@ namespace Sif.Framework.Demo.Uk.Provider
             GlobalConfiguration.Configuration.Services.Replace(typeof(IExceptionHandler), new GlobalUnexpectedExceptionHandler());
 
             Trace.TraceInformation("********** Application_Start **********");
+            log.Info("********** Application_Start **********");
             Register();
         }
         protected void Application_End(object sender, System.EventArgs e)
         {
             Trace.TraceInformation("********** Application_End **********");
+            log.Info("********** Application_End **********");
             Unregister();
         }
     }
