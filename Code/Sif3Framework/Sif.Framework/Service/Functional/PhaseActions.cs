5b3009e134891fc1f0c0d5595eaf5bd4f1be5706
Jon Nicholson <jon.nicholson@zinetdatasolutions.com>
2016-07-06 19:07:18 +0100

    Renamed classes

---
+++ Code/Sif3Framework/Sif.Framework/Service/Functional/PhaseActions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sif.Framework.Model.Infrastructure;
+using Sif.Framework.Model.Exceptions;
+
+namespace Sif.Framework.Service.Functional
+{
+    public class PhaseActions : IPhaseActions
+    {
+        public virtual string Create(Job job, Phase phase, string body = null, string contentType = null, string accept = null)
+        {
+            throw new RejectedException("Action not supported");
+        }
+
+        public virtual string Retrieve(Job job, Phase phase, string body = null, string contentType = null, string accept = null)
+        {
+            throw new RejectedException("Action not supported");
+        }
+
+        public virtual string Update(Job job, Phase phase, string body = null, string contentType = null, string accept = null)
+        {
+            throw new RejectedException("Action not supported");
+        }
+
+        public virtual string Delete(Job job, Phase phase, string body = null, string contentType = null, string accept = null)
+        {
+            throw new RejectedException("Action not supported");
+        }
+    }
+}
---
+++ Code/Sif3Framework/Sif.Framework/Service/Providers/ProviderService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sif.Framework.Model;
+using Sif.Framework.Model.Infrastructure;
+using Sif.Framework.Model.Query;
+
+namespace Sif.Framework.Service.Providers
+{
+    public abstract class ProviderService<T> : IBasicProviderService<T>
+    {
+        public abstract string GetServiceName();
+
+        public ServiceType GetServiceType()
+        {
+            return ServiceType.OBJECT;
+        }
+
+        public abstract void Startup();
+
+        public abstract void Shutdown();
+
+        public abstract T Create(T obj, bool? mustUseAdvisory = default(bool?), string zone = null, string context = null);
+
+        public abstract T Retrieve(string refId, string zone = null, string context = null);
+
+        public abstract List<T> Retrieve(uint? pageIndex = default(uint?), uint? pageSize = default(uint?), string zone = null, string context = null);
+
+        public abstract List<T> Retrieve(IEnumerable<EqualCondition> conditions, uint? pageIndex = default(uint?), uint? pageSize = default(uint?), string zone = null, string context = null);
+
+        public abstract List<T> Retrieve(T obj, uint? pageIndex = default(uint?), uint? pageSize = default(uint?), string zone = null, string context = null);
+
+        public abstract void Update(T obj, string zone = null, string context = null);
+
+        public abstract void Delete(string refId, string zone = null, string context = null);
+
+        public virtual void BroadcastEvents()
+        {
+        }
+    }
+}
