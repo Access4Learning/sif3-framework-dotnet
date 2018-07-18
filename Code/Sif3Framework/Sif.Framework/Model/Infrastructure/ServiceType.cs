2f18ea5883f09918dfc0f1d923bba4aca995d1af
rafidzal <rafidzal.rafiq@systemic.com.au>
2018-07-18 10:29:10 +0800

    Fix minor issue with setting a default ServiceType for request validation.

--- Code/Sif3Framework/Sif.Framework/Model/Infrastructure/ServiceType.cs
+++ Code/Sif3Framework/Sif.Framework/Model/Infrastructure/ServiceType.cs
@@ -1,12 +1,12 @@
 ﻿/*
- * Copyright 2014 Systemic Pty Ltd
- * 
+ * Copyright 2018 Systemic Pty Ltd
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,15 +16,13 @@
 
 namespace Sif.Framework.Model.Infrastructure
 {
-
     public enum ServiceType
     {
         UTILITY,
         OBJECT,
         FUNCTIONAL,
         SERVICEPATH,
-        XQUERYTEMP,
-        LATE
+        XQUERY,
+        TEMPLATE
     }
-
 }
