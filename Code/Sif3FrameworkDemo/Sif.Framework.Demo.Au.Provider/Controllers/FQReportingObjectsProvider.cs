9a115f8ad681b87fd0b7e1be223ab6a25dd5381a
rafidzal <rafidzal.rafiq@systemic.com.au>
2018-11-04 19:51:54 +0800

    Added demo Consumer and Provider for FQReportingObject objects.

---
+++ Code/Sif3FrameworkDemo/Sif.Framework.Demo.Au.Provider/Controllers/FQReportingObjectsProvider.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright 2018 Systemic Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Sif.Framework.Demo.Au.Provider.Models;
+using Sif.Framework.Demo.Au.Provider.Services;
+using Sif.Framework.Providers;
+using Sif.Framework.WebApi.ModelBinders;
+using System.Web.Http;
+
+namespace Sif.Framework.Demo.Au.Provider.Controllers
+{
+    public class FQReportingObjectsProvider : BasicProvider<FQReportingObject>
+    {
+        public FQReportingObjectsProvider() : base(new FQReportingObjectService())
+        {
+        }
+
+        [NonAction]
+        public override IHttpActionResult BroadcastEvents(string zoneId = null, string contextId = null)
+        {
+            return base.BroadcastEvents(zoneId, contextId);
+        }
+
+        [Route("~/api/FQReportingObjects/FQReportingObject")]
+        public override IHttpActionResult Post(FQReportingObject obj, [MatrixParameter] string[] zoneId = null, [MatrixParameter] string[] contextId = null)
+        {
+            return base.Post(obj, zoneId, contextId);
+        }
+    }
+}
